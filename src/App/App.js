8ba05e6a8fdfe2e2683b3ebc763fd4472d30507c
VyctorCosta <vyctorcosta00@gmail.com>
2022-01-22 21:18:34 -0300

    Arrumar css

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,11 @@
 import logo from '../logo.svg';
 import './App.css';
 import Header from '../Header/Header.js'
+import Body from '../Body/body';
 
 function App() {
   return (
-    <Header />
+    <Body />
   );
 }
 
---
+++ src/Body/body.js
@@ -0,0 +1,52 @@
+import './body.css'
+
+function Body() {
+    return (
+        <>
+        <ul>
+            <li>
+                <div className="seila">
+                    <img src="https://flagcdn.com/w320/de.png" alt="" />
+                    <h1>Germany</h1>
+                    <p><b>Population:</b> 81.770.900</p>
+                    <p><b>Region:</b> Europe</p>
+                    <p><b>Capital:</b> Berlin</p>
+                </div>
+            </li>
+            <li>
+                <div className="seila">
+                    <img src="https://flagcdn.com/w320/de.png" alt="" />
+                    <h1>Germany</h1>
+                    <p><b>Population:</b> 81.770.900</p>
+                    <p><b>Region:</b> Europe</p>
+                    <p><b>Capital:</b> Berlin</p>
+                </div>
+            </li>
+            {/*
+            <li>
+                <img src="https://flagcdn.com/w320/de.png" alt="" />
+                <h1>Germany</h1>
+                <p><b>Population:</b> 81.770.900</p>
+                <p><b>Region:</b> Europe</p>
+                <p><b>Capital:</b> Berlin</p>
+            </li>
+            <li>
+                <img src="https://flagcdn.com/w320/de.png" alt="" />
+                <h1>Germany</h1>
+                <p><b>Population:</b> 81.770.900</p>
+                <p><b>Region:</b> Europe</p>
+                <p><b>Capital:</b> Berlin</p>
+            </li>
+            <li>
+                <img src="https://flagcdn.com/w320/de.png" alt="" />
+                <h1>Germany</h1>
+                <p><b>Population:</b> 81.770.900</p>
+                <p><b>Region:</b> Europe</p>
+                <p><b>Capital:</b> Berlin</p>
+            </li> */}
+        </ul>
+        </>
+    );
+}
+
+export default Body;
