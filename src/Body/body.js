79ca8abe56c08a621336156d0d2398a36d81a64d
VyctorCosta <vyctorcosta00@gmail.com>
2022-01-25 02:42:57 -0300

    feat: Criaçao das tags utilizando request da API

--- src/Body/body.js
+++ src/Body/body.js
@@ -1,51 +1,60 @@
 import './body.css'
 
+function returnArrayElements(url) {
+    const request = new XMLHttpRequest();
+    request.open("GET", url, false);
+    request.send();
+    const string = request.responseText;
+    return JSON.parse(string);
+    
+}
+
+function returnUrl(region) {
+    if (!region) return "https://restcountries.com/v3.1/all";
+
+    return `https://restcountries.com/v3.1/region/${region}`
+}
+
+function createUlBody(array, name) {
+    const arrayLi = [];
+    let arrayDivs = [];
+    let div;
+    if (name) {
+        array = array.filter(element => element.startsWith(name))
+    }
+
+    array.forEach((element, index) => {
+        if (index !== 0 && index % 4 === 0) {
+            arrayLi.push(<li key={index*10}>{arrayDivs}</li>);
+            arrayDivs = [];
+        }
+
+        div = <div className="country-body" key={index}>
+            <img src={element.flags.png} />
+            <h1>{element.name.common}</h1>
+            <p><b>Population:</b> {element.population}</p>
+            <p><b>Region:</b> {element.region}</p>
+            <p><b>Capital:</b> {element.capital !== undefined ? element.capital[0] : "Sem capital"}</p>
+        </div>
+
+        arrayDivs.push(div)
+        if (element === array.slice(-1)[0]) {
+            arrayLi.push(<li key={index*10}>{arrayDivs}</li>);
+        }
+    })
+
+    return arrayLi;
+}
+
 function Body() {
+    const region = "oceania";
+    const name = ""
+    const url = returnUrl(region);
+    const arrayElements = returnArrayElements(url);
     return (
-        <>
-        <ul>
-            <li>
-                <div className="country-body">
-                    <img src="https://flagcdn.com/w320/de.png" alt="" />
-                    <h1>Germany</h1>
-                    <p><b>Population:</b> 81.770.900</p>
-                    <p><b>Region:</b> Europe</p>
-                    <p><b>Capital:</b> Berlin</p>
-                </div>
-                <div className="country-body">
-                    <img src="https://flagcdn.com/w320/de.png" alt="" />
-                    <h1>Germany</h1>
-                    <p><b>Population:</b> 81.770.900</p>
-                    <p><b>Region:</b> Europe</p>
-                    <p><b>Capital:</b> Berlin</p>
-                </div>
-                <div className="country-body">
-                    <img src="https://flagcdn.com/w320/de.png" alt="" />
-                    <h1>Germany</h1>
-                    <p><b>Population:</b> 81.770.900</p>
-                    <p><b>Region:</b> Europe</p>
-                    <p><b>Capital:</b> Berlin</p>
-                </div>
-                <div className="country-body">
-                    <img src="https://flagcdn.com/w320/de.png" alt="" />
-                    <h1>Germany</h1>
-                    <p><b>Population:</b> 81.770.900</p>
-                    <p><b>Region:</b> Europe</p>
-                    <p><b>Capital:</b> Berlin</p>
-                </div>
-            </li>
-            <li>
-                <div className="country-body">
-                    <img src="https://flagcdn.com/w320/de.png" alt="" />
-                    <h1>Germany</h1>
-                    <p><b>Population:</b> 81.770.900</p>
-                    <p><b>Region:</b> Europe</p>
-                    <p><b>Capital:</b> Berlin</p>
-                </div>
-            </li>
-        </ul>
-        </>
+        <ul>{createUlBody(arrayElements, name)}</ul>
     );
+
 }
 
 export default Body;
