979b949cb26ff903b8b7a381b417935d098d75fd
Rogerio Junior <rogerio.junior1016@gmail.com>
2022-01-31 13:21:43 -0300

    Countries details

--- src/Countries.js
+++ src/Countries.js
@@ -5,11 +5,12 @@ function countries() {
   return (
   <div className={style.countries}>
     <button className={style.back}>
+      <img src='https://cdn-icons-png.flaticon.com/512/109/109618.png' alt='Seta para esquerda' height={20} width={20} />
       <p>Go Back</p>
     </button>
     <div className={style.detailsBody}>
       <div className={style.imgCountry}>
-        <img src='https://cdn.britannica.com/47/6847-004-7D668BB0/Flag-Brazil.jpg' alt='' />
+        <img src='https://cdn.britannica.com/47/6847-004-7D668BB0/Flag-Brazil.jpg' alt='Bandeira do país' />
       </div>
       <div className={style.data}>
         <h2>Name</h2>
