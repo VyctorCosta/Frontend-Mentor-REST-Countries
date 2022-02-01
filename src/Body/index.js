cefee6e632214674a02cefab4dcfd4e85b482d51
VyctorCosta <vyctorcosta00@gmail.com>
2022-02-01 18:41:57 -0300

    refactor: Construindo uma api separada do Body

--- src/Body/index.js
+++ src/Body/index.js
@@ -1,63 +1,50 @@
-import React, { Component } from "react";
+import React from "react";
 import { useRouter } from "next/router";
 import styles from "./Body.module.css";
 
-function returnUrl(region) {
-    if (!region) return "https://restcountries.com/v3.1/all";
 
-    return `https://restcountries.com/v3.1/region/${region}`
-}
-
-function returnUlTag(array, name) {
+function createElement(array) {
+    if (array.length === 0) return array;
     const arrayLi = [];
-    let arrayDivs = [];
+    let arrayDiv = [];
     let div;
-
-    if (name) {
-        array = array.filter(element => element.name.common.toLowerCase().startsWith(name));
-    }
-
+    
     array.forEach((element, index) => {
-        if (index !== 0 && index % 4 === 0) {
-            arrayLi.push(<li className={styles.liBody} key={index*10}>{arrayDivs}</li>);
-            arrayDivs = [];
-        }
-
         div = <div className={styles.countryBody} key={index}>
             <img src={element.flags.png} />
             <h1>{element.name.common}</h1>
             <p><b>Population:</b> {element.population}</p>
             <p><b>Region:</b> {element.region}</p>
-            <p><b>Capital:</b> {element.capital !== undefined ? element.capital[0] : "Sem capital"}</p>
-        </div>
+            <p><b>Capital:</b> {element.capital !== undefined ? element.capital[0] : "No capital"}</p>
+        </div>;
+        arrayDiv.push(div);
 
-        arrayDivs.push(div)
-        if (element === array.slice(-1)[0]) {
-            arrayLi.push(<li className={styles.liBody} key={index*10}>{arrayDivs}</li>);
-        }
+        if (arrayDiv.length === 4) {
+            arrayLi.push(<li className={styles.liBody} key={index*10}>{arrayDiv}</li>);
+            arrayDiv = [];
+        } else if (element === array[array.length-1]) arrayLi.push(<li className={styles.liBody} key={index*10}>{arrayDiv}</li>);
     })
 
     return arrayLi;
 }
 
-async function request(url, setArrayCountries) {
-    const response = await fetch(url);
-    const arrayCountries = await response.json();
-    setArrayCountries(arrayCountries);
+async function getInfoApi(setArrayCountries, name, region) {
+    const response = await fetch(`/api/home?name=${name}&region=${region}`);
+    const array = await response.json();
+    setArrayCountries(array);
 }
+
 
 function Body() {
     const [arrayCountries, setArrayCountries] = React.useState([]);
-    const route = useRouter();
+    const [arrayUl, setArrayUl] = React.useState([]);
     const region = "asia";
-    const name = "mAl".toLowerCase()
-    const url = returnUrl(region);
-    request(url, setArrayCountries);
+    const name = "m".toLowerCase()
+    getInfoApi(setArrayCountries, name, region)
     
     return (
         <>
-            <ul className={styles.ulBody}>{returnUlTag(arrayCountries, name)}</ul>
-            
+            <ul>{createElement(arrayCountries)}</ul>            
         </>
     );
 }
--- src/api.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: "https://restcountries.com/v3.1/region/oceania"
-})
-
-export default api
